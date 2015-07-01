6c911aeffd4245a0f8d30ac67798ac120a4f69d0
Sukbeom Kim <chaoxifer@gmail.com>
2015-07-01 23:38:34 +0900

    edited mkcscope.sh

--- mkcscope.sh
+++ mkcscope.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -rf cscope.files cscope.out
-find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print > cscope.files
+find $PWD -type f \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print | awk '{print "\""$0"\""}' > cscope.files
 cscope -i cscope.files
