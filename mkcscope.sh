3c22a592824b920f2a8bcf1e2bcfce64db9a3ca3
Sukbeom Kim <chaoxifer@gmail.com>
2016-02-01 08:26:03 +0900

    Fix the issue on mkcscope.sh while using cscope and tramp-mode in emacs.

--- mkcscope.sh
+++ mkcscope.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 rm -rf cscope.files cscope.out
-find $PWD -type f \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print | awk '{print "\""$0"\""}' > cscope.files
+find $PWD -type f \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print | awk '{print "\""$0"\""}' | sed -e "s@$PWD/@./@g" > cscope.files
+
 cscope -i cscope.files
